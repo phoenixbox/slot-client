282e5001e8d5edaf29d7ca8a3a894b93d716a936
Shane Rogers <rogerssh@tcd.ie>
2015-09-12 19:04:12 -0700

    Assets directory structure

---
+++ test/index.js
@@ -0,0 +1,1 @@
+require("../app/views/app-test.js");
---
+++ test/unit/app/utils/helpers-test.js
@@ -0,0 +1,5 @@
+import {assert} from 'chai';
+import _ from 'lodash';
+import helpers from '../../../../app/utils/helpers.js';
+
+describe('#helpers', () => {});
