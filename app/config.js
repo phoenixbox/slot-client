4bbcf67d50886714247ef59c16f931c2fec12d8c
Shane Rogers <shane@repllabs.com>
2015-09-28 23:38:04 -0700

    Update rails server target name

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 /*eslint-env node */
-export const RAILSRoot = process.env.NODE_ENV === 'production' ? 'https://coffeeslots-server.herokuapp.com': 'http://127.0.0.1:4000';
+export const RAILSRoot = process.env.NODE_ENV === 'production' ? 'https://coffeeslots-rails.herokuapp.com': 'http://127.0.0.1:4000';
 export const HAPIRoot = process.env.NODE_ENV === 'production' ? 'https://coffeeslots-client.herokuapp.com': 'http://127.0.0.1:3800';
